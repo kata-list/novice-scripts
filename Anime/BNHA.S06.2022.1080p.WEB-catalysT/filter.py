e3615f9e1cdcb3bcdee497abbe131243c2047415
kata-list <katalist47@gmail.com>
2022-09-09 08:38:31 +0600

    Mockup added for BNHAS06 and ChainsawManS01.

---
+++ Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
@@ -0,0 +1,18 @@
+# TODO
+
+# import vapoursynth as vs
+
+# from vardautomation import X265, FileInfo
+
+# core = vs.core
+
+# CLIP = FileInfo('')
+
+# clip = CLIP.clip
+
+# filtered = 
+
+# if __name__ == '__main__':
+#     X265("x265-params.txt").run_enc(filtered, CLIP)   
+# else:
+#     clip.set_output(0)
