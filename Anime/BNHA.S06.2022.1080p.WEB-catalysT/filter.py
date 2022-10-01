58da1e1b9ffaf4a91a2f2ab4ec385902916bdfbb
kata-list <katalist47@gmail.com>
2022-10-01 20:08:19 +0600

    Y Plane : Descaled -> Upscaled -> Downscaled Back

--- Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
+++ Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
@@ -1,11 +1,20 @@
 import vapoursynth as vs
+import vsutil as u
+import vskernels as k
 
 from awsmfunc.base import bbmod
+from rvsfunc.NNEDI3 import NNEDI3
 
 core = vs.core
 
 clip = core.lsmas.LWLibavSource('bnha.s06e01.cr.mkv')
-dirt = bbmod(clip, 1, 1, 1, 1)
+clip = bbmod(clip, 1, 1, 1, 1)
+
+clip_y = u.get_y(clip)
+descale_y = k.Bilinear().descale(clip_y, 1280, 720)
+upscale_y = NNEDI3().rpow2(descale_y)
+to_1080p = k.BicubicAuto(c=-1/5).scale(upscale_y, 1920, 1080)
+join_yuv = core.std.ShufflePlanes([to_1080p, clip], [0, 1, 2], vs.YUV)
 
 clip.set_output(0)
-dirt.set_output(1)
+join_yuv.set_output(1)
