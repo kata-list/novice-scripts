10819333a8bcbcddceccf555b6c4fc8f2afbfa66
kata-list <katalist47@gmail.com>
2022-10-01 19:41:34 +0600

    Added Source -> Fixed Dirty Lines

--- Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
+++ Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
@@ -1,18 +1,11 @@
-# TODO
+import vapoursynth as vs
 
-# import vapoursynth as vs
+from awsmfunc.base import bbmod
 
-# from vardautomation import X265, FileInfo
+core = vs.core
 
-# core = vs.core
+clip = core.lsmas.LWLibavSource('bnha.s06e01.cr.mkv')
+dirt = bbmod(clip, 1, 1, 1, 1)
 
-# CLIP = FileInfo('')
-
-# clip = CLIP.clip
-
-# filtered = 
-
-# if __name__ == '__main__':
-#     X265("x265-params.txt").run_enc(filtered, CLIP)   
-# else:
-#     clip.set_output(0)
+clip.set_output(0)
+dirt.set_output(1)
