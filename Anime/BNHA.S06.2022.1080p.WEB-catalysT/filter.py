7a570b2f7db39829b2e9766137dc213b4fcf2954
kata-list <katalist47@gmail.com>
2022-10-10 17:01:01 +0600

    Reordered some filters + Stronger Mask

--- Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
+++ Anime/BNHA.S06.2022.1080p.WEB-catalysT/filter.py
@@ -14,37 +14,37 @@ core = vs.core
 def filter_chain(clip: vs.VideoNode) -> vs.VideoNode:
     
     clip = bbmod(clip, 1, 1, 1, 1)
-
-    clip_y = u.get_y(clip)
+    
+    clip32 = u.depth(clip, 32)
+    clip_y = u.get_y(clip32)
 
     descale_y = k.Bilinear().descale(clip_y, 1280, 720)
     upscale_y = NNEDI3().rpow2(descale_y)
 
     to_1080p = k.BicubicAuto(c=-1/4).scale(upscale_y, 1920, 1080)
 
-    dehalo = fine_dehalo(to_1080p, rx=2.3, darkstr=0, brightstr=0.9)
-
     descale_error = k.Bilinear().scale(descale_y, 1920, 1080)
-
     desc_mask = core.std.Expr([clip_y, descale_error], 'x y - abs')
-    desc_mask = desc_mask.std.Binarize(8)
+    desc_mask = desc_mask.std.Binarize(0.04)
     desc_mask = u.iterate(desc_mask, core.std.Maximum, 12)
     desc_mask = u.iterate(desc_mask, core.std.Inflate, 6)
 
-    masked_rescale = core.std.MaskedMerge(dehalo, clip_y, desc_mask)
+    masked_rescale = core.std.MaskedMerge(to_1080p, clip_y, desc_mask)
 
-    join_yuv = core.std.ShufflePlanes([masked_rescale, clip], [0, 1, 2], vs.YUV)
+    join_yuv = core.std.ShufflePlanes([masked_rescale, clip32], [0, 1, 2], vs.YUV)
 
     resc16 = u.depth(join_yuv, 16)
-    clip16 = u.depth(clip, 16)
+    
+    dehalo = fine_dehalo(resc16, rx=2.3, darkstr=0, brightstr=0.9)
 
-    denoise = DFTTest(clip16, ftype=1, sigma=1, backend=Backend.CPU())
+    denoise = DFTTest(dehalo, ftype=1, sigma=1, backend=Backend.CPU())
 
     deband = dumb3kdb(denoise, 16, 28, 15)
 
-    line_mask = FDoG().edgemask(clip_y).std.Maximum()
+    detail_mask = FDoG().edgemask(u.get_y(resc16), 15<<8, 15<<8)\
+                  .std.Maximum().std.Maximum().std.Minimum()
 
-    apply_mask = core.std.MaskedMerge(deband, resc16, u.depth(line_mask, 16))
+    apply_mask = core.std.MaskedMerge(deband, dehalo, detail_mask)
 
     final = u.depth(apply_mask, 10)
 
