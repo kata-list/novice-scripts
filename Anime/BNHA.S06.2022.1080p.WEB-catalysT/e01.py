cb0fb8562e195922e9b2c907659641f3f418677e
kata-list <katalist47@gmail.com>
2022-10-18 15:52:53 +0600

    Zoned and lowered ED bitrate .

--- Anime/BNHA.S06.2022.1080p.WEB-catalysT/e01.py
+++ Anime/BNHA.S06.2022.1080p.WEB-catalysT/e01.py
@@ -8,9 +8,11 @@ clip = SRC.clip
 
 filtered = filter_chain(clip)
 
+zones = {(31410, 33566): dict(b=0.3)}
+
 if __name__ == '__main__':
     SRC.name_clip_output = SRC.workdir / f'{SRC.name}.265'
-    X265('x265-params.txt').run_enc(filtered, SRC)
+    X265('x265-params.txt', zones={(31410, 33566): dict(b=0.3)}).run_enc(filtered, SRC)
 else:
     clip.set_output(0)
     filtered.set_output(1)
