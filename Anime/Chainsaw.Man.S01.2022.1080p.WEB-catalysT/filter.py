fa6518a3c3f740672f33c36a4af09fe6de2f45b3
kata-list <katalist47@gmail.com>
2022-10-19 13:41:39 +0600

    Rescaled + Masked native 1080p + Excluded op,ed .

--- Anime/Chainsaw.Man.S01.2022.1080p.WEB-catalysT/filter.py
+++ Anime/Chainsaw.Man.S01.2022.1080p.WEB-catalysT/filter.py
@@ -1,18 +1,37 @@
-# TODO
+import vapoursynth as vs
+import vstools as t
+import vskernels as k
+from rvsfunc.NNEDI3 import NNEDI3
 
-# import vapoursynth as vs
+core = vs.core
 
-# from vardautomation import X265, FileInfo
+clip = core.lsmas.LWLibavSource('chainsaw.man.s01e01.cr-web.mkv')
 
-# core = vs.core
+nh = 844
+nw = (16/9)*nh
 
-# CLIP = FileInfo('')
+clip32 = t.initialize_clip(clip, 32)
+clip_y = t.get_y(clip32)
+desc_y = k.Bilinear.descale(clip_y, nw, nh)
+upsc_y = NNEDI3().rpow2(desc_y)
+resc_y = k.Hermite.scale(upsc_y, 1920, 1080)
 
-# clip = CLIP.clip
+descale_error = k.Bilinear.scale(desc_y, 1920, 1080)
 
-# filtered = 
+desc_mask = core.akarin.Expr([clip_y, descale_error], 'x y - abs')
+desc_mask = desc_mask.std.Binarize(0.04)
+desc_mask = t.iterate(desc_mask, core.std.Maximum, 10)
+desc_mask = t.iterate(desc_mask, core.std.Inflate, 4)
 
-# if __name__ == '__main__':
-#     X265("x265-params.txt").run_enc(filtered, CLIP)   
-# else:
-#     clip.set_output(0)
+masked_resc = core.std.MaskedMerge(resc_y, clip_y, desc_mask)
+
+op_ed = [(3237, 5394), (34405, 35564)]
+
+exclude_op_ed = t.replace_ranges(masked_resc, clip_y, ranges=op_ed)
+
+resc_y = exclude_op_ed
+
+join_yuv = core.std.ShufflePlanes([resc_y, clip32], [0, 1, 2], vs.YUV)
+
+clip.set_output(0)
+join_yuv.set_output(1)
